ed11ff5aa1998acd1b1f14508d5eede869e7d6ff
afvv2602-U <adrian.fernandez.ceb@immune.institute>
2023-09-14 16:00:27 +0200

    Mejora de legibilidad

--- Cyber/Numeros Primos/cifrado.py
+++ Cyber/Numeros Primos/cifrado.py
@@ -1,47 +1,60 @@
-# cifrado.py
-
 # Definir alfabeto español
 ALFABETO = "ABCDEFGHIJKLMNÑOPQRSTUVWXYZ"
 
-# Función para encontrar el inverso multiplicativo de 'a' modulo 'm'
-def encontrar_inverso(a, m):
-    for i in range(1, m):  # Iteramos desde 1 hasta m-1
-        # Si i es el inverso multiplicativo de a modulo m
-        if (a * i) % m == 1:
-            return i  # Retornamos el inverso
-    return None  # Si no existe inverso, retornamos None
 
-def cifrar(mensaje, clave_decimacion, clave_desplazamiento):
+# Cifrado afín: C = (a * M + b) mod m
+# M representa la posición de la letra en el alfabeto (texto claro)
+# C representa la posición de la letra cifrada
+# a es la clave de decimación, debe ser coprimo con m (longitud del alfabeto)
+# b es la clave de desplazamiento
+def cifrar(mensaje, a, b):
     cifrado = ""
+    m = len(ALFABETO)  # m representa la longitud del alfabeto
     for letra in mensaje.upper():
         if letra in ALFABETO:
             M = ALFABETO.index(letra)
-            C = (clave_decimacion * M + clave_desplazamiento) % 27
+            C = (a * M + b) % m
             cifrado += ALFABETO[C]
         else:
-            cifrado += letra  # Para caracteres no presentes en ALFABETO, simplemente se añaden sin cambiar
+            cifrado += letra
     return cifrado
 
-def descifrar(mensaje_cifrado, p, q):
-    descifrado = ""  # Inicializamos el mensaje descifrado
-    inverso_p = encontrar_inverso(p, 27)  # Buscamos el inverso multiplicativo de p modulo 27
+
+# Descifrado afín: M = a_inv * (C - b) mod m
+# a_inv es el inverso multiplicativo de a mod m
+def descifrar(mensaje_cifrado, a, b):
+    descifrado = ""
+    m = len(ALFABETO)
+    a_inv = encontrar_inverso(a, m)
     
     for letra in mensaje_cifrado.upper():
         if letra in ALFABETO:
             C = ALFABETO.index(letra)
-            M = (inverso_p * (C - q)) % 27
+            M = (a_inv * (C - b)) % m
             descifrado += ALFABETO[M]
         else:
             descifrado += letra
     return descifrado
 
-def fuerza_bruta(mensaje_cifrado):
-    primos_menores_20 = [2, 3, 5, 7, 11, 13, 17, 19]  
-    resultados = []
 
-    for p in primos_menores_20:
-        if encontrar_inverso(p, 27):
-            for q in primos_menores_20:
-                descifrado = descifrar(mensaje_cifrado, p, q)
-                resultados.append((p, q, descifrado))
+# Función de fuerza bruta para encontrar posibles claves y mensajes descifrados
+def fuerza_bruta(mensaje_cifrado):
+    primos_menores_20 = [2, 3, 5, 7, 11, 13, 17, 19]
+    resultados = []
+    m = len(ALFABETO)
+    
+    for a in primos_menores_20:
+        if encontrar_inverso(a, m):
+            for b in range(m):  # b puede ser cualquier valor entre 0 y m-1
+                descifrado = descifrar(mensaje_cifrado, a, b)
+                resultados.append((a, b, descifrado))
     return resultados
+
+
+# Función para encontrar el inverso multiplicativo de 'a' modulo 'm'
+# Si existe un número i tal que (a * i) mod m = 1, entonces i es el inverso multiplicativo de a mod m
+def encontrar_inverso(a, m):
+    for i in range(1, m):  
+        if (a * i) % m == 1:
+            return i
+    return None
