b19297836c8bed3871baa3f58cc01ef7e095926a
afvv2602-U <adrian.fernandez.ceb@immune.institute>
2023-09-19 10:48:44 +0200

    RSA terminado

--- Cyber/RSA/code.py
+++ Cyber/RSA/code.py
@@ -1,27 +1,37 @@
 def main():
-    n, z = calculo_p_q(7, 17)
-
+    n, z = calculo_p_q(5, 17) # La multiplicacion de los dos numeros tiene que ser mayor que 27
+        
     # Clave publica
     k = coprimo(z)
 
     # Clave privada
     j = find_j(k, z)
 
-    mensaje = 'HOLASOYPEDROMEENCANTANADARYHACERSKATE'
+    print(f'valor n {n} valor de z {z} valor k {k} valor de j {j}')
 
-    mensaje_encrip = encriptar(mensaje, k, n)
+    # mensaje = 'HOLASOYPEDROMEENCANTANADARYHACERSKATE'
+    
+    mensaje = 'IMMUNEINSTITUTE'
+    ver_letras_limpias(mensaje)
+    cifrado_num,cifrado_letras = encriptar(mensaje, k, n)
 
-    print(f'Mensaje encriptado: {mensaje_encrip}')
+    print(f'Mensaje encriptado: {cifrado_num}{cifrado_letras}')
 
-    mensaje_desencr = desencriptar(mensaje_encrip, j, n)
+    mensaje_desencr = desencriptar(cifrado_num, j, n)
 
     print(f'Mensaje desencriptado: {"".join([ALFABETO[M] for M in mensaje_desencr])}')
 
 # Calcula y devuelve el n y z    
 def calculo_p_q(p, q):
+    # while True:
     n = p * q
     z = (p - 1) * (q - 1)
     return n, z
+
+# Ver las letras del mensaje original pasados a la posicion del alfabeto original
+def ver_letras_limpias(mensaje):
+    posiciones = [ALFABETO.index(letra) for letra in mensaje]
+    print(posiciones)
 
 # Encuentra y devuelve un numero coprimo con 'z' desde el rango 2 hasta z-1
 # Dos numeros son coprimos si su MCD es 1
@@ -50,16 +60,18 @@ def find_j(k, z):
         cont = cont + 1
         if j.is_integer():
             return int(j)
-
+    
 # convierte un mensaje de texto en una serie de numeros cifrados 
-# usando la clave publica k y el módulo n
+# usando la clave publica k y el modulo n
 def encriptar(mensaje, k, n):
     cifrado = []
+    cifrado_letras = []
     for letra in mensaje:
         M = ALFABETO.index(letra)
         C = pow(M, k, n) # Eleva M a k y hace el modulo de n
         cifrado.append(C)
-    return cifrado
+        cifrado_letras.append(ALFABETO[C % len(ALFABETO)]) # Le aplicamos a cada numero el mod 
+    return cifrado,cifrado_letras
 
 # Toma un mensaje cifrado y lo convierte de nuevo en texto 
 # Usando la clave privada j y el modulo n
