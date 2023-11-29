690a8abc4826f6967d4f826cfa0726c8580f46a7
afvv2602 <afvv26002@gmail.com>
2023-11-29 14:46:35 +0100

    update proyecto final

--- Data Science/Ejercicios/Machine Learning/Proyecto Final/main.py
+++ Data Science/Ejercicios/Machine Learning/Proyecto Final/main.py
@@ -69,18 +69,40 @@ plt.title('Análisis de Silueta para determinar el número óptimo de clusters')
 plt.show()
 
 
-#%% Creacion de los clusters
+#%% Creacion de los clusters 3D
+from mpl_toolkits.mplot3d import Axes3D
 
-for k in range(3,6):
-    kmeans = KMeans(n_clusters=k, random_state=42)
-    labels = kmeans.fit_predict(normalized_data)
-    # Creando el scatter plot de los clusters
+kmeans = KMeans(n_clusters=4, random_state=42)
+labels = kmeans.fit_predict(normalized_data)
+
+# Creando el scatter plot 3D de los clusters
+fig = plt.figure(figsize=(10, 10))
+ax = fig.add_subplot(111, projection='3d')
+ax.scatter(normalized_data[:, 0], normalized_data[:, 1], normalized_data[:, 2], c=labels, cmap='viridis', marker='o')
+centers = kmeans.cluster_centers_
+ax.scatter(centers[:, 0], centers[:, 1], centers[:, 2], c='crimson', s=400, alpha=1, marker='x')  # Centros más grandes
+ax.set_title('Visualización 3D de los clusters para k=4', fontsize=20)
+ax.set_xlabel('Banda de Edad Normalizada', fontsize=14, labelpad=15)
+ax.set_ylabel('Ingreso Mensual Normalizado', fontsize=14, labelpad=15)
+ax.set_zlabel('Educación Normalizada', fontsize=14, labelpad=15)
+plt.savefig('cluster_visualization.png', dpi=300) 
+plt.show()
+
+#%% Creacion de los clusters en 2D
+
+def plot_clusters(data, labels, centers, column_indices, title):
     plt.figure(figsize=(10, 6))
-    plt.scatter(normalized_data[:, 1], normalized_data[:, 2], c=labels, cmap='viridis', marker='o')
-    centers = kmeans.cluster_centers_
-    plt.scatter(centers[:, 1], centers[:, 2], c='red', s=200, alpha=0.75, marker='x')
-    plt.title(f'Visualización de los clusters para k={k}')
-    plt.xlabel('Ingreso Mensual Normalizado')
-    plt.ylabel('Educación Normalizada')
+    plt.scatter(data[:, column_indices[0]], data[:, column_indices[1]], c=labels, cmap='viridis', marker='o')
+    plt.scatter(centers[:, column_indices[0]], centers[:, column_indices[1]], c='red', s=200, alpha=0.75, marker='x')
+    plt.title(title)
+    plt.xlabel(datos_kmeans.columns[column_indices[0]])
+    plt.ylabel(datos_kmeans.columns[column_indices[1]])
     plt.show()
+    
+plot_clusters(normalized_data, labels, centers, [0, 1], 'Edad vs Ingreso Mensual')
+plot_clusters(normalized_data, labels, centers, [0, 2], 'Edad vs Educación')
+plot_clusters(normalized_data, labels, centers, [1, 2], 'Ingreso Mensual vs Educación')
+
+
+#%%
 
